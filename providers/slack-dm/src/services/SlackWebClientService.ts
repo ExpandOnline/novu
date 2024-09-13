275ced0a48d0a3b05524f65d45834fe2f7375db2
Daniel Santos <daniel.santos@deptagency.com>
2024-09-13 15:31:51 +0200

    fix: SlackWebClient

--- providers/slack-dm/src/services/SlackWebClientService.ts
+++ providers/slack-dm/src/services/SlackWebClientService.ts
@@ -3,7 +3,7 @@ import { WebClient } from '@slack/web-api';
 let instance: WebClient;
 
 export function getSlackWebClientInstance(token: string): WebClient {
-  if (instance.token != token) {
+  if (instance?.token != token) {
     instance = new WebClient(token);
   }
 
